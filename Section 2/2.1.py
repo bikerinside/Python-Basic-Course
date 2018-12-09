5922ba41e1e2111061ec9bf1523cf1c692741012
Vincenzo Rosario <vincenzor.casillo@gmail.com>
2018-12-09 13:06:53 +0100

    Create 2.1.py

---
+++ Section 2/2.1.py
@@ -0,0 +1,34 @@
+#Variabili
+
+Variabile = 10
+
+VariabileFloat = 2.4
+
+Var_Saluto = 'Ciao'
+
+print(Variabile)
+
+print(VariabileFloat)
+
+print(Var_Saluto)
+
+# le variaibli possono contenere:
+# interi, float, function,liste, dizionari
+
+a = 5
+
+b = 6
+
+c = a + b
+
+print(c)
+
+# il naming delle variabili in Python non
+# può iniziare con un numero, se proprio
+# ne avessimo l'esigenza, dovremmo
+# aggiungere un underscore _
+# ad esempio _100variabile
+
+_100var = 100
+
+print(_100var)
