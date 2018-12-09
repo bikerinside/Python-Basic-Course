8538cf875fb77faa7a943712b0e5f8e5195ec32a
Vincenzo Rosario <vincenzor.casillo@gmail.com>
2018-12-09 13:07:09 +0100

    Create 2.2.py

---
+++ Section 2/2.2.py
@@ -0,0 +1,32 @@
+
+'''
+Ciclo while
+
+Esempio 1
+
+il seguente ciclo itera
+finché i non vale 10
+'''
+
+i = 1
+
+while i <= 10:
+
+    print(i)
+
+    i += 1
+
+'''
+Esempio 2
+
+il seguente ciclo itera finché
+k non vale 0
+'''
+
+k = 10
+
+while k >= 0:
+
+    print(k)
+
+    k -= 1
