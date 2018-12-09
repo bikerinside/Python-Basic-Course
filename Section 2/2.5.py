f2aa6d801055c0d8a1b9efec8a0ca15855f6af86
Vincenzo Rosario <vincenzor.casillo@gmail.com>
2018-12-09 13:11:03 +0100

    Create 2.5.py

---
+++ Section 2/2.5.py
@@ -0,0 +1,37 @@
+'''
+istruzione condizionale
+
+if-else
+
+Esempio 1
+'''
+
+x = 15
+
+y = 7
+
+if x < y:
+# se x è minore di y allora stammpa :
+    print(x,'è minore di',y)
+# altrimenti stampa: 
+else:
+    print(x,'è maggiore di',y) 
+
+'''
+
+Esempio 2
+
+'''
+
+parola_1 = 'palla'
+
+parola_2 = 'pala'
+
+if parola_1 == parola_2:
+
+    print("Le parole sono uguali")
+
+else:
+
+    print("Le parole non sono uguali")
+
