b2fefbbc694dbf46acc068ddfa9c1f22f7eb9812
Vincenzo Rosario <vincenzor.casillo@gmail.com>
2018-12-09 13:07:26 +0100

    Create 2.3.py

---
+++ Section 2/2.3.py
@@ -0,0 +1,26 @@
+'''
+Ciclo for
+
+Esempio 1
+
+Il seguente esempio
+mostra un  uso semplice del ciclo for
+
+'''
+
+for i in range(1,11):
+
+    print(i)
+
+# la function range, crea una lista di 10 elementi da 1 a 10
+# il ciclo for grazie alla variabile i non fa altro che scorrere
+# la lista
+
+    
+# esempio lista :
+
+mia_lista = [0,2,6,8,10]
+
+# stampo la mia lista
+
+print(mia_lista)
