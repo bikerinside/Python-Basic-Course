f4f2335febb8aeb6d7d2e2613c8ed79019fc9695
bikerinside <vincenzor.casillo@gmail.com>
2018-12-09 12:47:19 +0100

    Add files via upload

---
+++ 2.4.py
@@ -0,0 +1,47 @@
+'''
+
+if-statement
+
+o
+
+istrutzione condizionale
+
+Esempio 1:
+
+'''
+
+x = 1 
+
+if x > 0: # se x è maggiore di 0 allora stampa true
+
+    print('true')
+
+
+if x < 0: # se x è minore di 0 allora stampa false
+
+    print('false')
+
+
+
+'''
+Esempio 2:
+
+'''
+a = 5
+
+b = 9
+
+c = 17
+
+if a < b < c: # se b è compreso tra a e c stampa il messaggio
+
+    print('b è compreso tra a e c')
+
+if a == b: # se a ha lo stesso valore di b allora stampa vero
+
+    print('true')
+
+if a != b # se a non ha lo stesso valore di b allora stampa vero
+
+    print('true')
+    
