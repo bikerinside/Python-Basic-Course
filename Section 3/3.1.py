81666fc7eccbdbccfbdc05463e79c9fdba4779fd
Vincenzo Rosario <vincenzor.casillo@gmail.com>
2018-12-10 22:24:18 +0100

    Create 3.1.py

---
+++ Section 3/3.1.py
@@ -0,0 +1,24 @@
+'''
+
+Funzioni o function in Python
+
+'''
+
+# esempio di funzione senza parametri
+
+def saluto():
+
+    print('Buongiorno')
+
+
+# la function main() è la function principale
+# di ogni programma Python, essa richiama tutte le altre function
+
+def main():
+    
+    saluto()
+
+
+
+
+
