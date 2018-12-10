d38d249087aa72dd871f53ec1f75c838b8bad924
bikerinside <vincenzor.casillo@gmail.com>
2018-12-10 08:50:10 +0100

    Create 2.6.py

---
+++ Section 2/2.6.py
@@ -0,0 +1,36 @@
+'''
+
+if elif else
+
+Esempio 1:
+
+'''
+
+x = 10
+
+y = 8
+
+z = 9
+
+k = 21
+
+
+if x > y and x > z:
+
+    print(x,'è maggiore di',y)
+
+elif x > z:
+
+    print(x,'è maggiore anche di',z)
+
+else:
+
+    print(x,'ma non maggiore di',k)
+
+
+'''
+
+verrà eseguita solo una di questa istruzioni ovvero
+la prima che sarà vera
+
+'''
